46f8888cd8dca20e0d21eba182ebbcffc6b68eb0
DBaldwin8 <darren_baldwin@live.com>
2024-10-02 15:27:26 +0100

    change slider to array to populate text

--- script.js
+++ script.js
@@ -1,42 +1,55 @@
 'use strict'
 
 const getPageViews = function (pageCountValue) {
-    if (pageCountValue === 0) {
+    if (pageCountValue == 0) {
         return '10k PAGEVIEWS';
-    } else if (pageCountValue ===  1) {
+    } else if (pageCountValue ==  1) {
         return '50K PAGEVIEWS';
-    } else if (pageCountValue === 2) {
+    } else if (pageCountValue == 2) {
         return '100K PAGEVIEWS';
-    } else if (pageCountValue === 3) {
+    } else if (pageCountValue == 3) {
         return '500K PAGEVIEWS';
-    } else if (pageCountValue === 4) {
+    } else if (pageCountValue == 4) {
         return '1M PAGEVIEWS';
     }
 }
 
+const pageViewStrings = [
+    '10k PAGEVIEWS',
+    '50K PAGEVIEWS',
+    '100K PAGEVIEWS',
+    '500K PAGEVIEWS',
+    '1M PAGEVIEWS',
+]
+
 const pageViewSelector = document.querySelector('#page-views');
 
 let pageViewElement = document.querySelector('#slider');
-
-let pageViewRetrieve = pageViewElement.addEventListener('change', () => {
-    let pageViews = getPageViews(pageViewElement.value);
+/*
+let pageViews = getPageViews(pageViewElement.value);
+console.log(pageViewElement.value);
+*/
+pageViewElement.addEventListener('change', () => {
+    let arrayIndex = pageViewElement.value
+    let pageViews = pageViewStrings[arrayIndex];
+    console.log(pageViews);
     pageViewSelector.textContent = `${pageViews}`;
 })
 
-const PRICE = function (pageViewsSelected,) {
+const PRICE = function (pageViewsSelected) {
     let price;
     
-    let yearlyDiscountRetrieve = document.querySelector('.checkbox').checked;
+    let yearlyDiscountRetrieve = document.querySelector('#toggle').checked;
 
-    if (pageViewsSelected === '10k') {
+    if (pageViewsSelected === 0) {
         price = 8;
-    } else if (pageViewsSelected ===  '50k') {
+    } else if (pageViewsSelected ===  1) {
         price = 12;
-    } else if (pageViewsSelected === '100k') {
+    } else if (pageViewsSelected === 2) {
         price = 16;
-    } else if (pageViewsSelected === '500k') {
+    } else if (pageViewsSelected === 3) {
         price = 24;
-    } else if (pageViewsSelected === '1M') {
+    } else if (pageViewsSelected === 4) {
         price = 36;
     }
 
@@ -47,9 +60,6 @@ const PRICE = function (pageViewsSelected,) {
     }
 }
 
-console.log(getPageViews('50k'));
-console.log(PRICE('50k', true));
 
 
 let pricePerSelector = document.querySelector('.price-per');
-pricePerSelector.textContent
