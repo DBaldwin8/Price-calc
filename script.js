ad8c4771221aa892e057248410c726c0138e9de9
DBaldwin8 <darren_baldwin@live.com>
2024-10-02 11:09:18 +0100

    added values to radios and started work on event listener on js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 'use strict'
 
+let element = document.querySelector('slider');
+element.addEventListener('change', () => {
+    return element.getAttribute('value');
+})
+
 const pageViews = function() {
     if (radio toggle id is 10k) {
-        return '10k PAGEVIEWS'
+        return '10k PAGEVIEWS';
     } ifelse (radio toggle id is 50k) {
-        return '50K PAGEVIEWS'
+        return '50K PAGEVIEWS';
     } ifelse (radio toggle id is 100k) {
-        return '50K PAGEVIEWS'
+        return '50K PAGEVIEWS';
     } ifelse (radio toggle id is 500k) {
-        return '50K PAGEVIEWS'
+        return '50K PAGEVIEWS';
     } ifelse (radio toggle id is 1M) {
-        return '1M PAGEVIEWS'
+        return '1M PAGEVIEWS';
     }
 }
 
@@ -18,19 +23,19 @@ const price = function() {
     let price = null;
     if (radio toggle id is 10k) {
         price = 8;
-    } ifelse (radio toggle id is 50k) {
+    } elseif (radio toggle id is 50k) {
         price = 12;
-    } ifelse (radio toggle id is 100k) {
+    } elseif (radio toggle id is 100k) {
         price = 16;
-    } ifelse (radio toggle id is 500k) {
+    } elseif (radio toggle id is 500k) {
         price = 24;
-    } ifelse (radio toggle id is 1M) {
-        price = 36
+    } elseif (radio toggle id is 1M) {
+        price = 36;
     }
 
     if (yearly billing is true) {
-        return price = (price*12)*0.75
+        return price = (price*12)*0.75;
     } else {
         return price;
+    }
 }
-
