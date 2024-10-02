8381861a04490442c1149c334e191fd68c8fce1e
DBaldwin8 <darren_baldwin@live.com>
2024-10-02 16:28:44 +0100

    yearly billing does update but reverts on changing slider.

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 'use strict'
 
-const getPageViews = function (pageCountValue) {
-    if (pageCountValue == 0) {
-        return '10k PAGEVIEWS';
-    } else if (pageCountValue ==  1) {
-        return '50K PAGEVIEWS';
-    } else if (pageCountValue == 2) {
-        return '100K PAGEVIEWS';
-    } else if (pageCountValue == 3) {
-        return '500K PAGEVIEWS';
-    } else if (pageCountValue == 4) {
-        return '1M PAGEVIEWS';
-    }
-}
-
 const pageViewStrings = [
     '10k PAGEVIEWS',
     '50K PAGEVIEWS',
@@ -22,44 +8,52 @@ const pageViewStrings = [
     '1M PAGEVIEWS',
 ]
 
-const pageViewSelector = document.querySelector('#page-views');
+const pricesArr = [
+    8,
+    12,
+    16,
+    25,
+    36
+]
 
-let pageViewElement = document.querySelector('#slider');
-/*
-let pageViews = getPageViews(pageViewElement.value);
-console.log(pageViewElement.value);
-*/
+const pageViewSelector = document.querySelector('#page-views');
+const pageViewElement = document.querySelector('#slider');
+
+const pricePerSelector = document.querySelector('#price-per');
+const monthOrYearSelector = document.querySelector('#month-or-year');
+
+const yearlyDiscountRetrieve = document.querySelector('#toggle')
+
 pageViewElement.addEventListener('change', () => {
     let arrayIndex = pageViewElement.value
     let pageViews = pageViewStrings[arrayIndex];
     console.log(pageViews);
     pageViewSelector.textContent = `${pageViews}`;
-})
+    let pricePer = pricesArr[arrayIndex];
+    pricePerSelector.textContent = `$${pricePer}.00`
 
-const PRICE = function (pageViewsSelected) {
-    let price;
-    
-    let yearlyDiscountRetrieve = document.querySelector('#toggle').checked;
-
-    if (pageViewsSelected === 0) {
-        price = 8;
-    } else if (pageViewsSelected ===  1) {
-        price = 12;
-    } else if (pageViewsSelected === 2) {
-        price = 16;
-    } else if (pageViewsSelected === 3) {
-        price = 24;
-    } else if (pageViewsSelected === 4) {
-        price = 36;
-    }
-
-    if (yearlyDiscountRetrieve) {
-        return price = (price*12)*0.75;
+    /*
+    if (!yearlyDiscountRetrieve.checked) {
+        pricePerSelector.textContent = `$${pricePer}.00`
+        monthOrYearSelector.textContent = '/month'
     } else {
-        return price;
+        pricePer = (pricePer*12)*0.75;
+        pricePerSelector.textContent = `$${pricePer}.00`
+        monthOrYearSelector.textContent = '/year'
     }
-}
+    */
+}) 
 
 
-
-let pricePerSelector = document.querySelector('.price-per');
+yearlyDiscountRetrieve.addEventListener('change', () => {
+    let arrayIndex = pageViewElement.value
+    let pricePer = pricesArr[arrayIndex]
+    if (!yearlyDiscountRetrieve.checked) {
+        pricePerSelector.textContent = `$${pricePer}.00`
+        monthOrYearSelector.textContent = '/month'
+    } else {
+        pricePer = (pricePer*12)*0.75;
+        pricePerSelector.textContent = `$${pricePer}.00`
+        monthOrYearSelector.textContent = '/year'
+    }
+})
